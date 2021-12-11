23ac27b237dcb8fb5a5359a8ae2a3380eb57775e
ChernyshevaVasilisa <2024chernysheva.vv@student.letovo.ru>
2021-12-11 10:32:20 +0300

    Сделана первая версия распознавания цифр и выведения их в питоне. Работает с перебоями

--- Распознавание.py
+++ Распознавание.py
@@ -33,39 +33,66 @@ def palm_size(landmark, shape):
     x2, y2 = landmark[5].x * shape[1], landmark[5].y * shape[0]
     return ((x1 - x2)**2 + (y1 - y2) **2) **.5
 
-def whatnum(gest):
-    numget = False
-    results = handsDetector.process(flippedRGB)
-    while not numget:
-        if results.multi_hand_landmarks is not None:
-             cv2.drawContours(flippedRGB, [get_points(results.multi_hand_landmarks[0].landmark, flippedRGB.shape)], 0, (255, 0, 0), 2)
-             (x, y), r = cv2.minEnclosingCircle(get_points(results.multi_hand_landmarks[0].landmark, flippedRGB.shape))
-             ws = palm_size(results.multi_hand_landmarks[0].landmark, flippedRGB.shape)
-             print(2 * r / ws)
-             if 2 * r / ws > 1.3:
-                 cv2.circle(flippedRGB,(int(x), int(y)), int(r), (0, 0, 255), 2)
-                 # кулак разжат
-                 prev_fist = False
-             else:
-                 cv2.circle(flippedRGB,(int(x), int(y)), int(r), (0, 255, 0), 2)
-                 if not prev_fist:
-                     # произошло сжимание
-                     print(0
-                     # Сейчас кулак зажат
-                     prev_fist = True
-        # Рисуем наш результат в каждом кадре, даже если рука не детектировалась
-        cv2.putText(flippedRGB, str(count), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), thickness=2)
 
- 
 scr = cv2.VideoCapture(0)
+handsDetector = mp.solutions.hands.Hands()
+count = 0
+prev_fist = False
 
 while(scr.isOpened()):
     ret, frame = scr.read()
     if cv2.waitKey(1) & 0xFF == ord('q') or not ret:
         break
     flipped = np.fliplr(frame)
-    flippedRGB = cv# Распознаем
+    flippedRGB = cv2.cvtColor(flipped, cv2.COLOR_BGR2RGB)
     results = handsDetector.process(flippedRGB)
+    if results.multi_hand_landmarks is not None:
+        numget = False
+        cv2.drawContours(flippedRGB, [get_points(results.multi_hand_landmarks[0].landmark, flippedRGB.shape)], 0, (255, 0, 0), 2)
+        (x, y), r = cv2.minEnclosingCircle(get_points(results.multi_hand_landmarks[0].landmark, flippedRGB.shape))
+        ws = palm_size(results.multi_hand_landmarks[0].landmark, flippedRGB.shape)
+        if 2 * r / ws > 1.3:
+            cv2.circle(flippedRGB,(int(x), int(y)), int(r), (0, 0, 255), 2)
+                # кулак разжат
+            prev_gest = -1
+                
+        else:
+            cv2.circle(flippedRGB,(int(x), int(y)), int(r), (0, 255, 0), 2)
+            if not prev_fist:
+                     # произошло сжимание
+                print(0)
+                     # Сейчас кулак зажат
+                prev_fist = True
+                prev_gest = 0
+                numget = True
+        if not numget:
+            ii = 0
+            while not numget and ii<8:
+                cv2.drawContours(flippedRGB, [get_points(results.multi_hand_landmarks[0].landmark, flippedRGB.shape)], 0, (255, 0, 0), 2)
+                (x, y), r = cv2.minEnclosingCircle(get_points(results.multi_hand_landmarks[0].landmark, flippedRGB.shape, ii))
+                ws = palm_size(results.multi_hand_landmarks[0].landmark, flippedRGB.shape)
+                if 2 * r / ws > 1.3:
+                    cv2.circle(flippedRGB,(int(x), int(y)), int(r), (0, 0, 255), 2)
+                        # кулак разжат
+                    prev_fist = False
+                        
+                else:
+                    cv2.circle(flippedRGB,(int(x), int(y)), int(r), (0, 255, 0), 2)
+                    if not prev_fist:
+                             # произошло сжимание
+                        if ii<3:
+                            print(ii+1)
+                        else:
+                            print(ii+2)
+                             # Сейчас кулак зажат
+                        prev_fist = True
+                        numget = True
+                ii+=1
+                
+    cv2.putText(flippedRGB, str(count), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), thickness=2)
+
+
+    
     res_image = cv2.cvtColor(flippedRGB, cv2.COLOR_RGB2BGR)
     cv2.imshow("Hands", res_image)
     
